b7de766180cbc51bf9c2d9aae896fff1188be484
ikaroony <104864014+ikaroony@users.noreply.github.com>
2023-10-05 16:12:48 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace celsius_converter
     {
         static void Main(string[] args)
         {
+            //creation date 9/6/2023
             Random rn = new Random();
             int cTemp = rn.Next(1, 51);
             double fTemp = (9.0 / 5.0) * cTemp + 32;
