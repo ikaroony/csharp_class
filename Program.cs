18a3c5a5e463b6c6ca9fdaea4857eeda0ff51779
ikaroony <104864014+ikaroony@users.noreply.github.com>
2023-10-05 16:10:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celsius_converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rn = new Random();
+            int cTemp = rn.Next(1, 51);
+            double fTemp = (9.0 / 5.0) * cTemp + 32;
+            Console.WriteLine(cTemp + " degrees Celsius is equivalent to " + fTemp + " degrees Farenheit.");
+            Console.ReadKey();
+        }
+    }
+}
